87ad2f030f4526a91996ce275b566c507a7355a3
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-08-09 14:26:39 +0000

    [Finish]フラッシュメッセージ、バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,20 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      flash.now[:alert] = "Failed to create book."
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -22,15 +26,26 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "Failed to update book."
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    if book.delete
+      flash[:notice] = "Book was successfully deleted."
+      redirect_to '/books'
+    else
+      @books = Book.all
+      flash.now[:alert] = "Failed to delete book."
+      render :index
+    end
   end
 
   private
